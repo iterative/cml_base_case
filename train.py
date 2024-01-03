17ce0dfb4eda8730decfb927459a5473463709b2
ronaldespinozaa <123993968+ronaldespinozaa@users.noreply.github.com>
2024-01-03 13:55:12 +0100

    nuevo modelo

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import os
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,12 +15,11 @@ X_test = np.genfromtxt("data/test_features.csv")
 y_test = np.genfromtxt("data/test_labels.csv")
 
 # Fit a model
-depth = 10
-clf = RandomForestClassifier(max_depth=depth, random_state=42, n_estimators=1000)
-clf.fit(X_train, y_train)
+penalty = "l2"
+lr=LogisticRegression(max_iter=1000, random_state=42,penalty=penalty)
+lr.fit(X_train, y_train)
 
-acc = clf.score(X_test, y_test)
-print(acc)
+acc=lr.score(X_test, y_test)
 
 # Save metrics to a text file
 metrics = f"Accuracy: {acc:10.4f}\n"
@@ -27,9 +27,8 @@ with open("metrics.txt", "w") as outfile:
     outfile.write(metrics)
 
 # Generate and save confusion matrix plot
-predictions = clf.predict(X_test)
-cm = confusion_matrix(y_test, predictions, labels=clf.classes_)
-disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=clf.classes_)
+cm=confusion_matrix(y_test, lr.predict(X_test),labels=lr.classes_)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=lr.classes_)
 disp.plot(cmap=plt.cm.Blues, values_format=".4g")  # Display the confusion matrix
 plt.title("Confusion Matrix")
 plt.savefig("plot.png")
